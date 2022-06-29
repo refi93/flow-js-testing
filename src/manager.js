e1717d9cac4b19d61405cd132f45781add779598
refi93 <rafael.korbas@gmail.com>
2022-06-29 10:41:29 +0200

    Add getTimeOffset/setTimeOffset utilities

--- src/manager.js
+++ src/manager.js
@@ -94,3 +94,20 @@ export const setBlockOffset = async (offset) => {
 
   return sendTransaction({ code, args, payer });
 };
+
+export const getTimestampOffset = async () => {
+  const FlowManager = await getManagerAddress();
+  const code = await registry.scripts.getBlockOffsetTemplate({ FlowManager });
+  return executeScript({ code });
+};
+
+export const setTimestampOffset = async (offset) => {
+  const FlowManager = await getManagerAddress();
+
+  const args = [offset];
+  const code = await registry.transactions.setBlockOffsetTemplate({ FlowManager });
+  const payer = [FlowManager];
+
+  return sendTransaction({ code, args, payer });
+};
+
